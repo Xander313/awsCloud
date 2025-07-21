f7b5352db8d112c6a7dd12e428110c336ae825d3
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-07-20 20:38:34 -0500

    Aws

--- HydroSys/settings.py
+++ HydroSys/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -95,10 +97,15 @@ WSGI_APPLICATION = 'HydroSys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mainDB',      # Reemplaza con el nombre de tu base
+        'USER': 'root',                   # O el usuario que uses en tu contenedor
+        'PASSWORD': 'admin',  # La contraseña configurada
+        'HOST': '172.22.0.2',              # O '127.0.0.1'
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
